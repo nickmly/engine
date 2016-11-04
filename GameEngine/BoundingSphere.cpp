4067d51b648a6f5097fd6e2fe10f0a469df995ea
Chenz13 <vincerauti@gmail.com>
2016-11-03 23:10:46 -0400

    Messsy collisions, proper physics
    Physics code works
    first commit with working collisons

--- GameEngine/BoundingSphere.cpp
+++ GameEngine/BoundingSphere.cpp
@@ -29,7 +29,8 @@ float BoundingSphere::GetRadius()
 bool BoundingSphere::Intersect(const BoundingSphere & other)
 {
 	float radiusDist = radius + other.radius;
-	float centerDist = (*other.center - *center).length();
+	glm::vec3 distVec = (*other.center - *center);
+	float centerDist = glm::sqrt((distVec.x * distVec.x) + (distVec.y * distVec.y) + (distVec.z * distVec.z));
 	if (centerDist < radiusDist) {
 		return true;
 		//return IntersectData(true, centerDist - radiusDist);
